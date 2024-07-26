8b32b14a005b5adeeb5224a33d53ca13d7da6358
kuyaLor <161716704+kuyaLor@users.noreply.github.com>
2024-07-26 21:12:08 +0800

    gaved up on this beast for now, ill try to visualize it on paper first

--- scripts.js
+++ scripts.js
@@ -1,27 +1,27 @@
-function box_valid(array, number, row, col) {
-    let startRow = row - (row % 3);
-    let startCol = col - (col % 3);
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (array[startRow + i][startCol + j] === number) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
 
-function col_valid(array, number, row, col) {
+let choices_array = [1,2,3,4,5,6,7,8,9]
+
+function choice(){
+  let random_index = Math.floor(Math.random() * choices_array.length);
+  return choices_array[random_index];
+}
+
+function assign(array, row, col, number){
+  // only call this when the to be assigned number is valid
+  array[row][col] = number;
+}
+
+function col_check(array, number,col) {
   for (let i = 0; i < 9; i++) {
-    if (array[i][col] == number) {
+    if (array[i][col] === number) {
       return false;
     }
   }
   return true;
 }
 
-function row_valid(array, number, row, col) {
-  for (i = 0; i <= 8; i++) {
+function row_check(array, number, row) {
+  for (let i = 0; i <= 8; i++) {
     if (array[row][i] === number) {
       return false;
     }
@@ -29,24 +29,21 @@ function row_valid(array, number, row, col) {
   return true;
 }
 
-function assign(array) {
-  for (let row = 0; row <= 8; row++) {
-    for (let col = 0; col <= 8; col++) {
-      let number = generate();
-      if ((row_valid(array, number, row, col)) && col_valid(array, number, row, col)) {
-        array[row][col] = number;
-      } else {
-        number = generate();
+function sub_grid_check(array, number, row, col) {
+  let startRow = row - (row % 3);
+  let startCol = col - (col % 3);
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (array[startRow + i][startCol + j] === number) {
+        return false;
       }
     }
   }
+  return true;
 }
 
-function generate() {
-  return Math.floor(Math.random() * 9) + 1; // Generate numbers between 1 and 9
-}
 
-let array = [
+ let board = [
   [5, 3, 0, 0, 7, 0, 0, 0, 0],
   [6, 0, 0, 1, 9, 5, 0, 0, 0],
   [0, 9, 8, 0, 0, 0, 0, 6, 0],
@@ -55,12 +52,28 @@ let array = [
   [7, 0, 0, 0, 2, 0, 0, 0, 6],
   [0, 6, 0, 0, 0, 0, 2, 8, 0],
   [0, 0, 0, 4, 1, 9, 0, 0, 5],
-  [0, 0, 0, 0, 8, 0, 0, 7, 9],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ];
 
-assign(array);
+let failed_choices = [];
 
-for (let row of array) {
-  let row_str = row.join(" ");
-  console.log(row_str);
+
+function make_choice(board){
+  let number = choice()
+  console.log(`make choice number is ${number}`);
+
+ for(let row=0; row< 9; row++){
+  for(let col=0; col< 9; col++){
+    if(col_check(board, number,col) && row_check(board, number, row), sub_grid_check(board, number, row, col)){
+     assign(board, row, col, number)
+    }
+    else{
+      failed_choices.push(number);
+    }
+  }
+ }
 }
+
+make_choice(board);
+
+console.log(board);
